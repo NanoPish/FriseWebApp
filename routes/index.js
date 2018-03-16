a9426912d035640c37b2bf9ca7e3cb8fc9e00644
paul <paul@belloc.ovh>
2018-03-16 18:17:18 +0000

    display events in index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'lafrise',
+        password : 'lafrise',
+        database : 'lafrise'
+    });
+
+    var events = [];
+    
+    connection.connect();
+
+    connection.query('SELECT * FROM events', function(err, rows, fields) {
+        if (err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"})
+        } else {
+            console.log('First event is : ', rows[0]);
+            for (var i = 0; i < rows.length; i++) {
+                events.push(rows[i]);
+            }
+            res.render('index', { title: 'Express', events: events });
+        }
+    });
+    
+    connection.end();
 });
 
 module.exports = router;
