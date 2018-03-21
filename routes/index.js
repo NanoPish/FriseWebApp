caf096fe6808ede6ad11ad02af2a78d35e13c97a
paul <paul@belloc.ovh>
2018-03-21 22:08:15 +0000

    filter data serverside, fix whitescreen, need do same with lol

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var vis = require('vis')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +13,8 @@ router.get('/', function(req, res, next) {
         database : 'lafrise'
     });
 
-    var events = [];
+    var wiki_events = [];
+    var lol_events = [];
     
     connection.connect();
 
@@ -20,11 +22,43 @@ router.get('/', function(req, res, next) {
         if (err) {
             res.status(500).json({"status_code": 500,"status_message": "internal server error"})
         } else {
-            console.log('First event is : ', rows[0]);
             for (var i = 0; i < rows.length; i++) {
-                events.push(rows[i]);
+                if (rows[i].source === "wikipedia" && (i % 300) === 0) 
+                    wiki_events.push(rows[i]);
+                else if (rows[i].source === "panda_score")
+                    lol_events.push(rows[i]);
             }
-            res.render('index.ejs', { title: 'Express', events: events });
+
+            i = 0;
+
+            var wiki_data = [];
+            var lol_data = [];
+
+            while (i < wiki_events.length) {
+                var event = {};
+                event['id'] = i;
+                event['content'] = wiki_events[i].description;
+                event['start'] =wiki_events[i].date;
+                wiki_data[i] = event;
+                i++;
+            }
+
+            i = 0;
+
+            while (i < lol_events.length) {
+                event = {};
+                event['id'] = i;
+                event['content'] = lol_events[i].description;
+                event['start'] =lol_events[i].date;
+                lol_data[i] = event;
+                i++;
+            }
+
+            var lol_dataset = new vis.DataSet(lol_data);
+
+            res.render('index.ejs', { title: 'Le Frise',
+                                      wiki_data: wiki_data,
+                                      lol_dataset: lol_dataset });
         }
     });
     
